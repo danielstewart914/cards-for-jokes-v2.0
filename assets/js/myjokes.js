95d473e694b70555dc5e20ddedcb050e24253e9d
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-24 08:02:43 -0700

    Refactored myjokes.js to us const, let, and arrow functions

--- assets/js/myjokes.js
+++ assets/js/myjokes.js
@@ -1,74 +1,73 @@
-var rootEl = $('#joke-container');
+const jokeContainerEl = $('#joke-container');
 
 /*
 Render jokes from local storage in a grid layout
 */
-function renderJokes() {
+const renderJokes = () => {
 
     // Create a row in the grid layout to display jokes
-    var jokesFrag = $( document.createDocumentFragment() );
-    var rowEl = $('<div>');
+    const jokesFrag = $( document.createDocumentFragment() );
+    const rowEl = $('<div>');
     rowEl.attr('class','row');
     jokesFrag.append(rowEl);
 
-    for (var iter = 0; iter < myJokes.length; iter++) {
+    for (let iter = 0; iter < myJokes.length; iter++) {
 
         // Create a column in a grid for each joke
-        var columnEl = $('<div>');
+        const columnEl = $('<div>');
         columnEl.attr('class','col s12 m6 l4');
         rowEl.append(columnEl);
 
         // Card element from materialize
-        var cardEl = $('<div>');
+        const cardEl = $('<div>');
         cardEl.attr('class', 'card');
-        cardEl.attr('id', 'joke-box');
         columnEl.append(cardEl);
 
-        var cardImageEl = $('<div>');
-        cardImageEl.attr('class' , 'card-image waves-effect waves-block waves-light');
+        const cardImageEl = $('<div>');
+        cardImageEl.attr('class' , 'card-image');
         cardEl.append(cardImageEl);
 
         // Add image to the card which when clicked opens the joke
-        var imageEl = $('<img>');
+        const imageEl = $('<img>');
         imageEl.attr ('class', 'activator');
         imageEl.attr ('src' , './assets/images/binary.jpeg');
         cardImageEl.append(imageEl);
 
         // Add content to the card
-        var cardContentEl = $('<div>');
+        const cardContentEl = $('<div>');
         cardContentEl.attr('class' , 'card-content');
         cardEl.append(cardContentEl);
 
-        var spanEl = $('<span>');
+        const spanEl = $('<span>');
         spanEl.attr('class' , 'card-title grey-text text-darken-4');
         cardContentEl.append(spanEl)
 
-        var iconEl = $('<i>');
+        const iconEl = $('<i>');
         iconEl.attr('class' , 'material-icons right');
         iconEl.text('more_vert');
         spanEl.append(iconEl);
 
         // To reveal the contents of the joke
-        var cardRevealEl = $('<div>');
+        const cardRevealEl = $('<div>');
         cardRevealEl.attr('class' , 'card-reveal');
         cardEl.append(cardRevealEl);
 
-        var spanRevealEl = $('<span>');
+        const spanRevealEl = $('<span>');
         spanRevealEl.attr('class' , 'card-title grey-text text-darken-4');
         cardRevealEl.append(spanRevealEl)
 
-        var iconRevealEl = $('<i>');
+        const iconRevealEl = $('<i>');
         iconRevealEl.attr('class' , 'material-icons right');
         iconRevealEl.text('close');
         spanRevealEl.append(iconRevealEl);
 
-        var jokeContentEl = $('<p>');
+        const jokeContentEl = $('<p>');
         if ( myJokes[iter].type === 1 ) jokeContentEl.text(myJokes[iter].joke);
         if ( myJokes[iter].type === 2 ) jokeContentEl.html( `<p>${ myJokes[iter].setup }</p><p>${ myJokes[iter].delivery }</p>` );
         
         // delete joke elements
-        var deleteEl = $( '<div>' ).addClass( 'delete-joke' ).data( 'index', iter );
-        var deleteIconEl = $( '<i>' ).addClass( 'material-icons right' ).text( 'cancel' ).data( 'index', iter );
+        const deleteEl = $( '<div>' ).addClass( 'delete-joke' ).data( 'index', iter );
+        const deleteIconEl = $( '<i>' ).addClass( 'material-icons right' ).text( 'cancel' ).data( 'index', iter );
         
         deleteEl.append( 'Delete ', deleteIconEl );
         jokeContentEl.append( deleteEl );
@@ -80,20 +79,20 @@ function renderJokes() {
 
 }
 
-rootEl.on( 'click', '.delete-joke', function( event ) {
+jokeContainerEl.on( 'click', '.delete-joke', ( event ) => {
 
     // get target
-    var target = $( event.target );
+    const target = $( event.target );
 
     // splice out index of clicked joke
     myJokes.splice( target.data( 'index' ), 1 );
 
     // save to local storage
-    localStorage.setItem("jokes", JSON.stringify(myJokes));
+    localStorage.setItem( 'jokes', JSON.stringify( myJokes ) );
 
     // rerender jokes
-    rootEl.html( renderJokes() );
+    jokeContainerEl.html( renderJokes() );
 
 } );
 
- rootEl.html( renderJokes() );
+ jokeContainerEl.html( renderJokes() );
