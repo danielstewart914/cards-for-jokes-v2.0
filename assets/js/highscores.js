340683031443218f20e5363399cc192960ae9d2d
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-24 07:21:57 -0700

    rewrote highscore rendering

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,24 +1,28 @@
-const highScores = JSON.parse(localStorage.getItem( 'scoreBoard' ) );
-
-const renderScores = () => {
-  highScores.forEach( ( { name, score } ) => {
-
-  $( '#highScores' ).append( 
-    `<tr>
-      <td>${name}</td>
-      <td>${score}</td>
-    </tr>` );
-
-  } );
+// highs scores loaded from local storage
+let highScores = JSON.parse(localStorage.getItem( 'scoreBoard' ) );
+// high scores display element <tbody> on html page
+const highScoresEl = $( '#highScores' );
+// render high scores to screen
+const renderScores = ( scores ) => {
+  // document fragment for rendered high scores
+  const highScoreFrag = $( document.createDocumentFragment() );
+  // append high score table rows for each high score
+  scores.forEach( highScore  => highScoreFrag.append( `<tr><td>${ highScore.name }</td><td>${ highScore.score }</td></tr>` ) );
+  // return fragment
+  return highScoreFrag;
 }
 
-// clear scores when clear scores button is clicked
+  // clear scores when clear scores button is clicked
 $( '#clear-scores' ).on( 'click', () => {
 
-  localStorage.removeItem( 'scoreBoard' );
-
-  location.reload();
+  // clear high scores
+  highScores = [];
+  // rerender high scores
+  highScoresEl.html( renderScores( highScores ) );
+  // save empty array to local storage
+  localStorage.setItem( 'scoreBoard', highScores );
 
 } );
 
-renderScores();
+// render highs cores on page load
+highScoresEl.html( renderScores( highScores ) );
